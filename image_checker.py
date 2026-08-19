238d08af6b62803ddd1bb1176b993f062c9607d1
Josephine <josephine.jane.gapuz@eee.upd.edu.ph>
2026-08-20 03:10:47 +0800

    Orientation

--- image_checker.py
+++ image_checker.py
@@ -1,3 +1,16 @@
 from PIL import Image
-img = Image.open("images/pm_en_1-3-010_f.jpg")
-img.show()
+import os
+
+folder = "images"
+
+for filename in os.listdir(folder):
+    if filename.endswith(".jpg"):
+        path = os.path.join(folder, filename)
+        img = Image.open(path)
+
+        # Apply rotation if needed, e.g. 90 degrees
+        rotated = img.rotate(90, expand=True)
+
+        # Strip EXIF by converting to RGB
+        rotated = rotated.convert("RGB")
+        rotated.save(path)
