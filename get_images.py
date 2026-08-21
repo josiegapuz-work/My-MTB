657bab2e7a3ce2e5db2238f40446c36e091b590f
Josie <josephine.gapuz.work@gmail.com>
2026-08-21 20:42:21 +0800

    testing tabs

--- get_images.py
+++ get_images.py
@@ -15,7 +15,7 @@ def show_types(types: List[str]):
         with c:
             try:
                 if img_path.exists():
-                    st.image(str(img_path), width=100)
+                    st.image(str(img_path), width=416)
                 else:
                     st.caption(t)
             except Exception:
